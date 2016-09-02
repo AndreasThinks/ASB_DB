7040180cd9a8fb865692c0544982465e488869a6
Andreas Varotsis <andrea.varotsis@gmail.com>
2016-09-02 16:06:27 +0100

    database schema up and running

---
+++ app/forms.py
@@ -0,0 +1,13 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired
+
+class LoginForm(Form):
+    openid = StringField('openid', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
+
+class SearchForm(Form):
+    firstName = StringField('firstName')
+    surname = StringField('surname')
+    dateOfBirth = StringField('dateOfBirth')
+    identifier = StringField('identifier')
