6cb4a04d0731a619fedc2152742e7291195843c4
Andreas Varotsis <andrea.varotsis@gmail.com>
2016-09-02 21:44:20 +0100

    basic search functionality implemented

--- app/forms.py
+++ app/forms.py
@@ -6,8 +6,16 @@ class LoginForm(Form):
     openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
 
+    def __init__(self, *args, **kwargs):
+        kwargs['csrf_enabled'] = False
+        super(LoginForm, self).__init__(*args, **kwargs)
+
 class SearchForm(Form):
     firstName = StringField('firstName')
     surname = StringField('surname')
     dob = StringField('dob')
     identifier = StringField('identifier')
+
+    def __init__(self, *args, **kwargs):
+        kwargs['csrf_enabled'] = False
+        super(SearchForm, self).__init__(*args, **kwargs)
--- app/views.py
+++ app/views.py
@@ -1,15 +1,45 @@
 from flask import render_template
-from app import app
+from app import app, db
 from .forms import LoginForm, SearchForm
+from .models import Subjects, Interactions
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     form = SearchForm()
+    subjects = Subjects.query.all()
+    subject_list = []
+    for subject in subjects:
+        subject_attributes = [subject.firstName, subject.surname, subject.dob, subject.identifier]
+        subject_list.append(subject_attributes)
+    if form.validate_on_submit():
+        subject_list = []
+        '''for subject in subjects:
+            subject_attributes = [subject.firstName, subject.surname, subject.dob, subject.identifier]
+            subject_list.append(subject_attributes)'''
+        if form.firstName.data != "":
+            subjects = Subjects.query.filter_by(firstName=form.firstName.data)
+        if form.surname.data !="":
+            subjects = Subjects.query.filter_by(surname=form.surname.data)
+        if form.dob.data !="":
+            subjects = Subjects.query.filter_by(dob=form.dob.data)
+        if form.identifier.data !="":
+            subjects = Subjects.query.filter_by(data=form.identifier.data)
+        for subject in subjects:
+            subject_attributes = [subject.firstName, subject.surname, subject.dob, subject.identifier]
+            subject_list.append(subject_attributes)
+        return render_template("search_subject.html", subjects=subject_list)
     return render_template("index.html",
+                           subjects = subject_list,
                            title='Home',
                            form=form)
+
+
+@app.route('/search_subject', methods=['GET', 'POST'])
+def search_subject():
+    return render_template("search_subject.html")
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
